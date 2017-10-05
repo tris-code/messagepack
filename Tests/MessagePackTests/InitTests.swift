77b2655612950730f9939c7a30f19a36b488de80
Tony Freeman <tonyfreeman@users.noreply.github.com>
2017-10-05 09:53:03 +0000

    [Tests] Fix InitTests

--- Tests/MessagePackTests/InitTests.swift
+++ Tests/MessagePackTests/InitTests.swift
@@ -1,43 +1,30 @@
-///*
-// * Copyright 2017 Tris Foundation and the project authors
-// *
-// * Licensed under the Apache License, Version 2.0 (the "License");
-// * you may not use this file except in compliance with the License
-// *
-// * See LICENSE.txt in the project root for license information
-// * See CONTRIBUTORS.txt for the list of the project authors
-// */
-//
-//import Test
-//import MessagePack
-//
-//class InitTests: TestCase {
-//    func testUnsafeBufferPointer() {
-//        let expected: MessagePack = [1,2,3]
-//        let bytes: [UInt8] = [0x93, 0x01, 0x02, 0x03]
-//        let buffer = UnsafeRawBufferPointer(start: bytes, count: bytes.count)
-//        var reader = MessagePackReader(buffer: buffer)
-//        guard let decoded = try? reader.decode() as MessagePack else {
-//            fail("decode error")
-//            return
-//        }
-//        assertEqual(decoded, expected)
-//    }
-//
-//    func testUnsafePointer() {
-//        let expected: MessagePack = [1,2,3]
-//        let bytes: [UInt8] = [0x93, 0x01, 0x02, 0x03]
-//        var reader = MessagePackReader(bytes: UnsafeRawPointer(bytes), count: 4)
-//        guard let decoded = try? reader.decode() as MessagePack else {
-//            fail("decode error")
-//            return
-//        }
-//        assertEqual(decoded, expected)
-//    }
-//
-//    static var allTests = [
-//        ("testUnsafeBufferPointer", testUnsafeBufferPointer),
-//        ("testUnsafePointer", testUnsafePointer)
-//    ]
-//}
+/*
+ * Copyright 2017 Tris Foundation and the project authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License
+ *
+ * See LICENSE.txt in the project root for license information
+ * See CONTRIBUTORS.txt for the list of the project authors
+ */
+
+import Test
+import MessagePack
+
+class InitTests: TestCase {
+    func testByteStream() {
+        let expected: MessagePack = [1,2,3]
+        let bytes: [UInt8] = [0x93, 0x01, 0x02, 0x03]
+        var reader = MessagePackReader(InputByteStream(bytes))
+        guard let decoded = try? reader.decode() as MessagePack else {
+            fail("decode error")
+            return
+        }
+        assertEqual(decoded, expected)
+    }
+
+    static var allTests = [
+        ("testByteStream", testByteStream),
+    ]
+}
 
